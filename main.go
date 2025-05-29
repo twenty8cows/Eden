800491cb454fb2df994e35c25b3e586643300100
Twenty8cows <139688457+twenty8cows@users.noreply.github.com>
2025-05-29 14:14:34 -0400

    Updated West Palm Beach zone
    Added Pembroke Pines area to WPB delivery Zone.

--- main.go
+++ main.go
@@ -487,7 +487,7 @@ map.on('moveend', updateLabelVisibility);
 func main() {
 	godotenv.Load()
 
-	kmlFile := "/Users/jon/fl_map_go/Eden Layout 05.13.25.kml"
+	kmlFile := "/Users/jon/fl_map_go/Eden Layout 05.29.25.kml"
 	roadsGeoJSON := os.Getenv("ROADWAYS_GEOJSON")
 	mapboxToken := os.Getenv("MAPBOX_TOKEN")
 
